6e0eb2bda8c43f78b4f4f80d1f08d4b83015c9c5
robbe.hires <rhires@zomedica.com>
2023-03-04 22:21:44 -0500

    backend API

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "campappamplifytest01": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "campappamplifytest01f2bb5c99": {
       "AppClientID": "string",
